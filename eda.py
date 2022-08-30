791e5d616561c8d0b632a4be25045a39f0b7df83
guillermina <suttersg@gmail.com>
2022-08-30 21:36:02 +0200

    EDA update

--- eda.py
+++ eda.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 # read csv
 df = pd.read_csv('LondonStageFull.csv', on_bad_lines='skip')  # get rid of tokenizing data error
@@ -32,6 +33,8 @@ print(7 * '--------')
 
 # keep rows where 'c_Performer' is not NaN
 df = df[df['c_Performer'].notna()]
+print('Length after dropping NaN:', len(df['c_Performer']))
+print(7 * '--------')
 
 # separate list of comma-separate performers into elements of list
 df['c_Performer'] = [i.split(',') for i in df['c_Performer']]
@@ -40,5 +43,15 @@ df['c_Performer'] = [i.split(',') for i in df['c_Performer']]
 df = df.explode('c_Performer')
 performers_clean = df['c_Performer'].value_counts()
 print(performers_clean)
+print('Length with explode:', len(df['c_Performer']))
 print(7 * '--------')
 
+# type of performance
+p_type = df['p_PType'].value_counts()
+print(p_type)
+print(7 * '--------')
+
+# count how many performance types per month
+df['date'] = df['e_EventDate'].apply(lambda x: pd.to_datetime(int(x), errors='coerce', format='%Y%m%d'))
+print(df['date'].to_list()[:20])
+print(type(df['e_EventDate'].to_list()[0]))
